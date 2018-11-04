39c85efa905a52935bc0f2ecc18c48dceadc8495
Felix Yin <31239925+FelixMundial@users.noreply.github.com>
2018-11-05 02:11:33 +0800

    test commit on WebStorm

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <slide-show :slides="slides" :inv="invTime"></slide-show>
   </div>
 </template>
 
 <script>
+import slideShow from 'vueslideshow'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      invTime: 2000,
+      slides: [
+        {
+          src: require('../assets/nature.jpg'),
+          title: '测试1',
+          href: '/about'
+        },
+        {
+          src: require('../assets/city-lights.jpg'),
+          title: '测试2',
+          href: '/about'
+        }
+      ]
     }
+  },
+  components: {
+    slideShow
   }
 }
 </script>
